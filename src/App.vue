783a7a03190746491df459988700f881ab045175
Simon Alvarado <s.alvarado@viseven.com>
2023-07-31 13:32:38 -0300

    update(*): first push

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="game d-flex flex-column align-items-center justify-content-between">
+    <h1 class="game__title pb-3 pt-4">Memory Game</h1>
+    <div v-if="!userName" class="name-container">
+      <input type="text" v-model="playerName" class="name-container__input form-control mb-2" placeholder="Ingresa tu nombre">
+      <button @click="startGame" class="name-container__button btn btn-primary">Inicar Juego</button>
+    </div>
+    <div v-else class="board">
+      <div class="row">
+        <MemoryCard v-for="(card, index) in shuffledCards" :key="index" :id="card.id" :image="card.image" :flipped="card.flipped" @select-card="flipCard"/>
+      </div>
+    </div>
+    <div v-if="userName">
+      <p class="board__scoreboard">
+        Correctas: {{ correct }} | Incorrectas: {{ wrong }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import MemoryCard from './components/MemoryCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MemoryCard
+  },
+  data () {
+    return {
+      playerName: '',
+      userName: '',
+      cards: [],
+      selectedCards: [],
+      correct: 0,
+      wrong: 0
+    }
+  },
+  computed: {
+    shuffledCards() {
+      const sortedCards = this.cards.slice(-24)
+      return sortedCards.sort(() => Math.random() - 0.5)
+    }
+  },
+  methods: {
+    startGame(){
+      this.userName = this.playerName
+      this.getCards()
+    },
+    getCards() {
+      const endpoint = 'https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20'
+      axios.get(endpoint)
+        .then((response) =>{
+          const apiData = response.data.entries
+
+          const memoryCards = apiData.flatMap((animal) => [
+              { id: animal.meta.uuid + '_1', position: animal.meta.uuid + '_1', name: animal.meta.name, image: animal.fields.image.url, flipped: false, found: false },
+              { id: animal.meta.uuid + '_2', position: animal.meta.uuid + '_2', name: animal.meta.name, image: animal.fields.image.url, flipped: false, found: false }
+            ])
+          
+          this.cards = memoryCards
+        })
+        .catch((error) => {
+          console.error('Error fetching cards:', error)
+        })
+    },
+    flipCard(cardId) {
+      console.log('card id', cardId)
+      const cardToFlip = this.cards.find(card => card.id === cardId)
+
+      if (!cardToFlip || cardToFlip.flipped || this.selectedCards.length >= 2) {
+        return;
+      }
+
+      cardToFlip.flipped = true
+      this.selectedCards.push(cardToFlip)
+
+      if (this.selectedCards.length === 2) {
+        this.checkMatch()
+      }
+    },
+    checkMatch() {
+      const [firstCard, secondCard] = this.selectedCards
+
+      if (firstCard.name === secondCard.name) {
+        firstCard.found = true
+        secondCard.found = true
+        this.selectedCards = []
+        this.correct++
+      } else {
+        setTimeout(() => {
+          firstCard.flipped = false
+          secondCard.flipped = false
+          this.wrong++
+          this.selectedCards = []
+        }, 1000)
+      }
+    }
   }
 }
 </script>
@@ -21,6 +108,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100vw;
+}
+.game{
+  height: 100vh;
+}
+.name-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+}
+.name-container__input{
+  max-width: 250px;
+  margin: 0 auto;
+}
+.board {
+  width: 76%;
+  max-width: 850px;
+}
+@media (min-width: 1200px) {
+  .board {
+    width: 55%
+  }
+}
+@media (max-width: 768px) {
+  .board {
+    width: 90%
+  }
 }
 </style>
---
+++ src/components/MemoryCard.vue
@@ -0,0 +1,77 @@
+<template>
+    <div @click="selectCard" class="col-3 col-md-2 col-lg-2 col-xl-2 mb-4 d-flex justify-content-center">
+        <div class="card--container">
+            <div v-if="flipped" class="card__face card__face--front">
+                <img :src="image" alt="Memory Card" class="card__face--image">
+            </div>
+            <div v-else class="card__face card__face--back">
+               <div class="card__face--back-text">?</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        flipped: {
+            type: Boolean,
+            required: false
+        }
+    },
+    setup(props, context) {
+    const selectCard = () => {
+      context.emit('select-card', props.id); // Pass the id of the clicked card
+    };
+
+    return {
+      selectCard
+    };
+  }
+}
+</script>
+
+<style scoped>
+.card--container{
+    width: 100%;
+    height: 0;
+    padding-top: 100%;
+    position: relative;
+    cursor: pointer;
+}
+.card__face{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    backface-visibility: hidden;
+    transition: transform 0.5s ease;
+    border-radius: 5px;
+}
+.card__face--back{
+    background-color: rgb(10, 34, 55);
+    color: white;
+}
+.card__face--back-text{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    font-size: 2rem;
+}
+.card__face--image{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 5px;
+}
+</style>
